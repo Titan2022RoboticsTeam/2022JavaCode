5651a86ccb1e2c8f39f72e2168924979fbd1aef5
byfu34 <bmbg439@yahoo.com>
2016-02-14 13:35:40 -0600

    Commented robot map ports

--- FRC2016/src/org/usfirst/frc/team2022/robot/RobotMap.java
+++ FRC2016/src/org/usfirst/frc/team2022/robot/RobotMap.java
@@ -4,18 +4,26 @@ import edu.wpi.first.wpilibj.AnalogInput;
 
 public class RobotMap {
 
+	// ports for drive base
 	public static int leftDrivePort = 0;
 	public static int rightDrivePort = 0;
+	
+	// encoder ports for drive base
 	public static int leftEncoderA = 0;
 	public static int leftEncoderB = 0;
 	public static int rightEncoderA = 0;
 	public static int rightEncoderB = 0;
+	
+	// shooter ports
 	public static int frontIntakePort = 0;
 	public static int backIntakePort = 0;
 	public static int hingePort = 0;
+	
+	// shooter sensor ports 
 	public static int ballLimitSwitchPort = 0;
-	public static int shooterLimitSwitchPort = 0;
 	public static int potentiometerPort = 0;
+	
+	// encoder ports for shooter
 	public static int rightShooterEncoderPortA = 0;
 	public static int rightShooterEncoderPortB = 0;
 	public static int leftShooterEncoderPortA = 0;
